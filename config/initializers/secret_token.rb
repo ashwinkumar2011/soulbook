dc20e24a0697ac8b817a486e913e19a323f59445
OpenShift guest <ashwillwink@yahoo.com>
2013-06-17 13:08:16 -0400

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soulbook::Application.config.secret_token = 'be824a8504231855e31febe6876abd066a1715e61875b6d498ae36424fca18bcf1ebcda19ede62d5fccab48f4118d0d63f8a5011b49977189d57aeaae7a10391'
